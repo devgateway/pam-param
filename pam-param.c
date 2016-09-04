4d979b97743e103002db175e0342f8469fcab31b
Styopa Semenukha <ssemenukha@developmentgateway.org>
2016-09-04 17:34:50 -0400

    Wrote module boilerplate

--- pam-param.c
+++ pam-param.c
@@ -2,16 +2,49 @@
 
 #include <unistd.h>
 #include <limits.h>
+#include <sys/types.h>
+#include <ldap.h>
 
 #include "pam-param.h"
 #include "inih/ini.h"
 
-int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	char hostname[HOST_NAME_MAX];
-	int rc;
+/* returns NULL on failure */
+config *config_read(const char *filename) {
+}
 
-	rc = gethostname(hostname, HOST_NAME_MAX);
-	if (rc) return PAM_BUF_ERR;
+/* return object count or LDAP_FAIL */
+int count_entries(LDAP *ld, const ldap_query *query) {
+}
+
+/* get host name, optionally chopped at first dot; return 0 on success */
+int get_host_name(char *host_name) {
+	int rc = gethostname(host_name, HOST_NAME_MAX);
+	if (rc) return rc;
+
+	/* TODO: chop */
+
+	return 0;
+}
+
+int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+	int rc;
+	char host_name[HOST_NAME_MAX];
+
+	config *cfg = config_read(CONFIG_FILE);
+	if (!cfg) return PAM_BUF_ERR;
+
+	/* TODO: get user name from PAM */
+
+	/* TODO: connect to LDAP */
+
+	/* TODO: check if is super admin */
+
+	rc = get_host_name(host_name);
+	if ( !rc ) return PAM_BUF_ERR;
+
+	/* TODO: check if access permitted */
+
+	/* TODO: disconnect from LDAP */
 
 	return PAM_SUCCESS;
 }
--- pam-param.h
+++ pam-param.h
@@ -1,5 +1,25 @@
-#define PAM_SM_ACCOUNT
-
 #include <pam_modules.h>
 
+#define PAM_SM_ACCOUNT
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv);
+
+typedef struct {
+	char *base;
+	int scope;
+	char *filter;
+} ldap_query;
+
+typedef struct {
+	int short_name;
+	const char *ldap_url;
+	const char *ldap_user;
+	const char *ldap_pass;
+	ldap_query admin;
+	ldap_query member;
+} config;
+
+#ifndef CONFIG_FILE
+#define CONFIG_FILE "/etc/pam_param.ini"
+#endif
+
+#define LDAP_FAIL -1
