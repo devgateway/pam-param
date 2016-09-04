3d714e34c977046b2ee795be8165b9e6967c99f2
Styopa Semenukha <ssemenukha@developmentgateway.org>
2016-09-04 16:11:30 -0400

    Moved includes to the source file

--- pam-param.c
+++ pam-param.c
@@ -1,4 +1,10 @@
+#define _XOPEN_SOURCE 700
+
+#include <unistd.h>
+#include <limits.h>
+
 #include "pam-param.h"
+#include "inih/ini.h"
 
 int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 	char hostname[HOST_NAME_MAX];
--- pam-param.h
+++ pam-param.h
@@ -1,8 +1,5 @@
 #define PAM_SM_ACCOUNT
-#define _XOPEN_SOURCE 700
 
 #include <pam_modules.h>
-#include <unistd.h>
-#include <limits.h>
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv);
