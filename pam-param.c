6bdc6748c61e6391ce197bdffe736a2f773267a6
Beshad Talayeminaei <btalayeminaei@developmentgateway.org>
2016-09-06 16:51:30 -0400

    parse ini file

--- pam-param.c
+++ pam-param.c
@@ -8,8 +8,61 @@
 #include "pam-param.h"
 #include "inih/ini.h"
 
+static int handler (void* user, const char* section, const* char name, const char* value) {
+	config* pconfig = (config *)user;
+	#define SECTION(s, n) strcmp(s,section)==0
+	#define NAME(n) strcmp(n,name)==0
+	if (SECTION("")) {
+		if (NAME("short_name")) {
+			pconfig->short_name = atoi(value);
+		}
+	} else if (SECTION("ldap")) {
+		if (NAME("uri")) {
+			pconfig->ldap_uri = strdup(value);
+		} else if (NAME("binddn")) {
+			pconfig->ldap_dn = strdup(value);
+		} else if (NAME("bindpw")) {
+			pconfig->ldap_pw = strdup(value);
+		} else {
+			return 0;
+		}
+	} else {
+		ldap_query *q;
+
+		if (SECTION("admin")) {
+			q = &(pconfig->admin);
+		} else if (SECTION("user")) {
+			q = &(pconfig->user);
+		} else if (SECTION("host")) {
+			q = &(pconfig->host);
+		} else if (SECTION("membership")) {
+			q = &(pconfig->membership);
+		} else {
+			return 0;
+		}
+
+		if (NAME("base")) {
+			q->base = strdup(value);
+		} else if (NAME("scope")) {
+			q->scope = atoi(value);
+		} else if (NAME("filter")) {
+			q->filter = strdup(value);
+		} else {
+			return 0;
+		}
+	}
+	return 1;
+
+}
+
 /* returns NULL on failure */
 config *config_read(const char *filename) {
+	config conf;
+	if (parse_ini(CONFIG_FILE, handler, &conf) < 0 ) {
+		prinft("Can't load configuration file:%s\n",INI_FILE);
+		return NULL;
+	}
+	return *conf;
 }
 
 /* return object count or LDAP_FAIL */
