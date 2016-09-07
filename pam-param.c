94cb1372b6719a633c4d32f0a438dae974ed2993
Beshad Talayeminaei <btalayeminaei@developmentgateway.org>
2016-09-07 17:05:40 -0400

    updated bind functions to not use deprecated ones

--- pam-param.c
+++ pam-param.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <ldap.h>
 #include <stdlib.h>
+#include <lber.h>
 
 #include "pam-param.h"
 #include "inih/ini.h"
@@ -125,7 +126,11 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 	rc = ldap_initialize(ld, cfg.ldap_uri);
 	if (rc != LDAP_SUCCESS) return rc;
 
-	rc = ldap_simple_bind_s(ld, cfg.ldap_dn, cfg.ldap_pw);
+	struct berval cred;
+	cred.bv_val = cfg.ldap_pw;
+	cred.bv_len = strlen(cfg.ldap_pw);
+
+	rc = ldap_sasl_bind_s(ld, cfg.ldap_dn, LDAP_SASL_SIMPLE, &cred, NULL, NULL, NULL);
 	if (rc != LDAP_SUCCESS) return rc;
 
 	/* TODO: check if is super admin */
@@ -137,7 +142,7 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 	/* TODO: check if access permitted */
 
 	/* TODO: disconnect from LDAP */
-	rc = ldap_unbind(ld);
+	rc = ldap_unbind_ext(ld, NULL, NULL);
 	if (rc != LDAP_SUCCESS) return rc;
 
 	return PAM_SUCCESS;
