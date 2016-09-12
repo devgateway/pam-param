f1f0455a3591e8333210ae820ad20095139e0ca3
Beshad Talayeminaei <btalayeminaei@developmentgateway.org>
2016-09-12 12:24:32 -0400

    updated is_super_admin

--- pam-param.c
+++ pam-param.c
@@ -151,20 +151,38 @@ void complete_ldap_query (ldap_query q, char *a, char *b) {
 
 /*returns 1 if user is super admin*/
 int is_super_admin (LDAP *ld) {
-    char *user_dn;
+    char *user_dn = NULL;
     int rc;
+    int count;
+    int result;
     ldap_query q = cfg.admin;
 	LDAPMessage *res;
 
     rc = get_dn(ld, cfg.user, user_dn);
-    if (rc !=0 ) return rc;
+    if (rc !=TRUE) {
+        result = ERROR;
+        goto end;
+    }
     complete_ldap_query (q, user_dn);
 
 	rc = ldap_search_ext_s(ld, q.base, q.scope, q.filter, LDAP_NO_ATTRS, 1, NULL, NULL, NULL, LDAP_NO_LIMIT, &res);
-    if (ldap_count_entries(ld, res) != 1) return LDAP_INAPPROPRIATE_AUTH;
+    if (rc != LDAP_SUCCESS ) {
+        result = ERROR;
+        goto end;
+    }
 
-    ldap_mem_free(user_dn);
-    return 1;
+    count = ldap_count_entries(ld, res);
+    switch(count){
+        case 0: result = FALSE; goto end;
+        case 1: result = TRUE; goto end;
+        default: result = ERROR; goto end;
+    }
+
+    end:
+        if (user_dn) ldap_mem_free(user_dn);
+        return result;
+
+
 }
 
 /*returns 1 if user is permitted*/
