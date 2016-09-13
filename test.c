267c7a0ae15757eb88a9ad5730a634436dbd18c6
Styopa Semenukha <ssemenukha@developmentgateway.org>
2016-09-13 11:34:06 -0400

    Reverted test

--- test.c
+++ test.c
@@ -14,9 +14,6 @@ extern config cfg;
 int main(int argc, const char *argv[]) { 
 	int rc;
 	char host_name[HOST_NAME_MAX];
-	const char **user_name;
-	LDAP *ld;
-	struct berval cred;
 
 	if (argc != 2) {
 		fprintf(stderr, "One numeric argument required: test number.\n");
@@ -30,24 +27,13 @@ int main(int argc, const char *argv[]) {
 			rc = ini_parse(CONFIG_FILE, handler, NULL);
 			return rc ? FAIL : PASS;
 
-            /* get user name from PAM */
-            rc = pam_get_user(pamh, user_name, NULL);
-			return rc ? FAIL : PASS;
+			/* TODO: get user name from PAM */
 
 			/* TODO: connect to LDAP */
-            /* connect to LDAP */
-            rc = ldap_initialize(&ld, cfg.ldap_uri);
-			return rc ? FAIL : PASS;
-
-            cred.bv_val = cfg.ldap_pw;
-            cred.bv_len = strlen(cfg.ldap_pw);
-            rc = ldap_sasl_bind_s(ld, cfg.ldap_dn, LDAP_SASL_SIMPLE, &cred, NULL, NULL, NULL);
-			return rc ? FAIL : PASS;
 
 			/* TODO: check if is super admin */
 
 		case 1:
-            /*to test escaped use: string = "\\this*is(a)test\0";*/
 			rc = gethostname(host_name, HOST_NAME_MAX);
 			if (rc) return FAIL;
 			if (cfg.short_name) shorten_name(host_name, HOST_NAME_MAX);
@@ -55,10 +41,7 @@ int main(int argc, const char *argv[]) {
 
 			/* TODO: check if access permitted */
 
-            /*disconnect from LDAP */
-            rc = ldap_unbind_ext(ld, NULL, NULL);
-			return rc ? FAIL : PASS;
-
+			/* TODO: disconnect from LDAP */
 		default:
 			fprintf(stderr, "Invalid test number: %i.\n", test);
 			return FAIL;
