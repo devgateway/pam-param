7a421d94b45f766640f010c78bc2271ab3aaa08e
Styopa Semenukha <ssemenukha@developmentgateway.org>
2016-09-19 13:20:27 -0400

    Added func stubs as per PAM module guide

--- pam-param.h
+++ pam-param.h
@@ -3,6 +3,22 @@
 #define PAM_SM_ACCOUNT
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv);
 
+/* stubs: in case the admin mistakenly calls the module */
+#define NOT_IMPLEMENTED(f) \
+PAM_EXTERN int f(pam_handle_t *pamh, int flags) { \
+	return PAM_SERVICE_ERR; \
+}
+
+#define PAM_SM_AUTH
+NOT_IMPLEMENTED(pam_authenticate)
+
+#define PAM_SM_PASSWORD
+NOT_IMPLEMENTED(pam_chauthtok)
+
+#define PAM_SM_SESSION
+NOT_IMPLEMENTED(pam_open_session)
+NOT_IMPLEMENTED(pam_close_session)
+
 typedef struct {
 	char *base;
 	int scope;
