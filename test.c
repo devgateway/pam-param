3d77aef65baeaa64aa358e7011a66d62dbe3bbd0
Styopa Semenukha <ssemenukha@developmentgateway.org>
2016-09-21 23:28:38 -0400

    Wrote the test utility

--- test.c
+++ test.c
@@ -9,9 +9,20 @@
 int main() {
 	char username[32 + 1];
 	int rc;
+	struct pam_conv conv;
+	pam_handle_t *pamh;
 
 	for (;;) {
 		printf("Enter user name: ");
-		if ( scanf("%32s", &username) != 1 ) return 1;
+		if ( scanf("%32s", &username) != 1 ) return 0;
+
+		rc = pam_start(SERVICE_NAME, username, &conv, &pamh);
+		if (rc != PAM_SUCCESS) return 1;
+
+		rc = pam_acct_mgmt(pamh, 0);
+		puts( pam_strerror(pamh, rc) );
+
+		rc = pam_end(pamh, rc);
+		if (rc != PAM_SUCCESS) return 1;
 	}
 }
