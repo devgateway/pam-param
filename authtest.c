8c7bb816b6e56baf5cc370e0fd3a8e97123df51d
Styopa Semenukha <ssemenukha@developmentgateway.org>
2016-09-28 14:33:22 -0400

    Added non-interactive test mode

--- authtest.c
+++ authtest.c
@@ -6,23 +6,41 @@
 #define SERVICE_NAME "pam_param_test"
 #endif
 
-int main() {
-	char username[32 + 1];
-	int rc;
+int try_acct(const char *username) {
+	int rc, auth_code;
 	struct pam_conv conv;
 	pam_handle_t *pamh;
 
-	for (;;) {
-		printf("Enter user name: ");
-		if ( scanf("%32s", &username) != 1 ) return 0;
+	rc = pam_start(SERVICE_NAME, username, &conv, &pamh);
+	if (rc != PAM_SUCCESS) goto fail;
 
-		rc = pam_start(SERVICE_NAME, username, &conv, &pamh);
-		if (rc != PAM_SUCCESS) return 1;
+	auth_code = pam_acct_mgmt(pamh, 0);
+	fprintf(stderr, "%s\n", pam_strerror(pamh, auth_code));
 
-		rc = pam_acct_mgmt(pamh, 0);
-		puts( pam_strerror(pamh, rc) );
+	rc = pam_end(pamh, rc);
+	if (rc == PAM_SUCCESS) return auth_code;
 
-		rc = pam_end(pamh, rc);
-		if (rc != PAM_SUCCESS) return 1;
+fail:
+	fprintf(stderr, "PAM stack failed: %s\n", pam_strerror(pamh, rc));
+	return rc;
+}
+
+int main(int argc, const char *argv[]) {
+	char username[32 + 1];
+
+	switch (argc) {
+		case 1: /* interactive */
+			for (;;) {
+				printf("Enter user name: ");
+				if ( scanf("%32s", &username) != 1 ) return 0;
+				try_acct(username);
+			}
+
+		case 2: /* non-interactive */
+			return try_acct(argv[1]);
+
+		default:
+			fprintf(stderr, "Usage: %s [USERNAME]\n", argv[0]);
+			return -1;
 	}
 }
